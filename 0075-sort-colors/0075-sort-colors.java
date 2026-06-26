3fc2c54409a139d4960a7513d2b681a7d08bc9f4
krishnakant0028 <kkpatel0028@gmail.com>
2026-06-26 17:43:27 +0530

    Time: 5 ms (2.75%), Space: 43.8 MB (10.31%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        
+        int n = nums.length;
+
+        Arrays.sort(nums);
+
+        System.out.println(Arrays.toString(nums));
+        
+        }
+
+
+    }
