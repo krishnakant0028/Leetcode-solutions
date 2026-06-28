a2a90711f2b631499f79a39119a1941d00406a0c
krishnakant0028 <kkpatel0028@gmail.com>
2026-06-29 04:56:10 +0530

    Time: 36 ms (40.01%), Space: 46.8 MB (83.36%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
+         int n = nums.length;
         int [] ans = new int[2];
         for(int i = 0;i < n;i++){
             for(int j = i+1;j < n;j++){
@@ -12,5 +12,6 @@ class Solution {
         }return ans;
 
         
+       
     }
 }
