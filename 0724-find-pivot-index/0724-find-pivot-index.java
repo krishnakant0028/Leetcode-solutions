af133ef033c9349eab68cc1a730262b78cbd36f0
krishnakant0028 <kkpatel0028@gmail.com>
2026-06-30 06:25:17 +0530

    Time: 1 ms (98.56%), Space: 47.2 MB (88.35%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int [] prefix = new int [nums.length];
+        prefix[0] = nums[0];
+
+        for (int i = 1;i <nums.length;i++){
+            prefix[i] = prefix[i-1]+nums[i];
+
+        }
+        for (int i =0;i < nums.length;i++){
+            int left;
+            int right;
+             if (i ==0){
+                left =0;
+             }else{
+                left = prefix[i-1];
+
+             }
+             right = prefix[nums.length-1] - prefix[i];
+             if (left ==right){
+                return i;
+             }
+        } 
+        return -1;    
+    }
+}
