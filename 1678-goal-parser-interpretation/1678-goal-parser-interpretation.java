da49d650b8b214a55a2329e64ea810b4f5013383
krishnakant0028 <kkpatel0028@gmail.com>
2026-07-09 04:39:37 +0530

    Time: 1 ms (75.92%), Space: 43.1 MB (17.03%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String interpret(String command) {
+
+        command = command.replace("(al)","al");
+
+        command =  command.replace("()","o");
+
+        return command;
+        
+    }
+}
