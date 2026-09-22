9247262c490a0c3a24c5fa89406a786d32aaf570
krishnakant0028 <kkpatel0028@gmail.com>
2026-09-23 04:12:57 +0530

    Time: 9 ms (41.85%), Space: 42 MB (70.1%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int fib(int n) {
+        if (n ==0 || n ==1){
+            return n;
+        }
+        return fib(n-1) + fib(n-2);}
+        public static void main (String [] args){
+            Solution kk = new Solution();
+            System.out.println(kk.fib(6));
+        }
+
+
+
+    }
+        
+    
